47567ea432b5184c6dfdec680144833b47e01e80
Rick Henry <fredericmhenry@gmail.com>
2018-05-23 02:58:27 -0400

    Posting basically works
    The creation of posts and associated tag and category files has basically
    been implemented. Files are created, added to git, and committed. Pushing
    needs to be added (as well as possible configuring the branch)
    
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- cliblog/cli.py
+++ cliblog/cli.py
@@ -1,8 +1,9 @@
 import datetime
 import os
 from pathlib import Path
-import sys
+import subprocess
 import click
+import git
 from ruamel.yaml import YAML
 
 
@@ -23,15 +24,31 @@ def cli(ctx):
 @click.option("-t", "--tag", multiple=True)
 @click.pass_context
 def post(ctx, title, category, tag):
+    repo = git.Repo(ctx.obj['path'])
     today = datetime.datetime.today()
-    filename = today.strftime("%Y-%m-%d") + '-' + '-'.join(title.split())
+    new_post = today.strftime("%Y-%m-%d") + '-' + '-'.join(title.split()) + '.md'
     for atag in tag:
-        tagpage = Path(ctx.obj['path'], 'tag', atag + '.html')
-        if not tagpage.exists():
-            with tagpage.open('w') as tagfile:
+        tag_page = Path(ctx.obj['path'], 'tag', atag + '.html')
+        if not tag_page.exists():
+            with tag_page.open('w') as tagfile:
                 tagfile.write(f"---\nlayout: tag\ntag: {atag}\n---")
-    categorypage = Path(ctx.obj['path'], 'category', category + '.html')
-    if not categorypage.exists():
-        with categorypage.open('w') as categoryfile:
+            repo.index.add([str(tag_page)])
+    category_page = Path(ctx.obj['path'], 'category', category + '.md')
+    if not category_page.exists():
+        with category_page.open('w') as categoryfile:
             categoryfile.write("---\nlayout: category"
                                f"\ncategory: {category}\n---")
+        repo.index.add([str(category_page)])
+    postpath = Path(ctx.obj['path'], '_posts', new_post)
+    tags = ' '.join(tag)
+    with postpath.open('w') as postfile:
+        postfile.write("---\n")
+        postfile.write(f"title: {title}\n")
+        postfile.write("layout: post\n")
+        postfile.write(f"category: {category}\n")
+        postfile.write(f"tags: {tags}\n")
+        postfile.write("---\n")
+    editor = os.environ['EDITOR'] or 'vim'
+    subprocess.run([editor, postpath])
+    repo.index.add([str(postpath)])
+    repo.index.commit(f"Add Post {title} with tags {tags} and category {category}")
